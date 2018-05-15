d2abc57b591b1ba718301d57fcbdeacb5b0dd690
John Firth <johfirth@gmail.com>
2018-05-14 21:00:33 -0400

    html tweeks, API stuff

---
+++ app/data/friends.js
@@ -0,0 +1,47 @@
+const friendName = {
+  first: {
+      "name":"Bill",
+      "scores":[
+          5,
+          1,
+          4,
+          4,
+          5,
+          1,
+          2,
+          5,
+          4,
+          1
+        ]
+  },
+  second: {
+    "name":"Jimmy",
+    "scores":[
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5
+    ]
+  },
+  third: {
+    "name": "Jill",
+    "scores": [
+      5,
+      4,
+      5,
+      4,
+      3,
+      2,
+      1,
+      5,
+      5,
+      4
+    ]
+  }
+}
